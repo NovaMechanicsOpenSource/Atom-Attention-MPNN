62075beab406b649258750cf13e4d6eb2e2aca98
NovaMechanics Open Source <opensource@novamechanics.com>
2024-08-26 15:41:12 +0300

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,5 +1,8 @@
+"""Loads a trained model checkpoint and makes predictions on a dataset."""
+
 from chemprop.args import PredictArgs
 from chemprop.make_predictions import make_predictions
+
 
 if __name__ == "__main__":
     make_predictions(args=PredictArgs().parse_args())
--- pretraining.py
+++ pretraining.py
@@ -1,7 +1,8 @@
 """Pretraining a model on a dataset."""
 
 from chemprop.args import TrainArgs
-from chemprop.pretraining_mpn import pre_train
+from chemprop.test_pretraining import pre_train
+
 
 if __name__ == '__main__':
     pre_train(args=TrainArgs().parse_args())
